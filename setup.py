83becf200f6dfd534561f221e31db0136f42b0b5
Haim Daniel <haim@ma-gen.com>
2018-06-05 13:49:48 +0300

    setup.py: commented out

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
+from os import path as osp
 
-from setuptools import setup, find_packages
-from os.path import join, dirname
+import setuptools
 
-print "TODO: fix me"
-#setup(
-#    name = 'pyubertooth',
-#    version = '0.1',
-#    description = 'Pure Python Library for Ubertooth',
-#    author='hackgnar',
-#    packages = find_packages(),
-#    long_description = open(join(dirname(__file__), 'README.md')).read(),
-#    scripts = ['tools/ubertooth_dump.py', 'pyubertooth/ubertooth.py'],
-#    install_requires = [
-#        'pyusb'
-#    ],
-#    package_data = {
-#        'pyubertooth': []
-#    }
-#)
+setuptools.setup(
+    name='pyubertooth',
+    version='0.1',
+    description='Pure Python Library for Ubertooth',
+    author='hackgnar',
+    packages=setuptools.find_packages(),
+    long_description=open(osp.join(osp.dirname(__file__), 'README.md')).read(),
+    scripts=['tools/ubertooth_dump.py', 'pyubertooth/ubertooth.py'],
+    install_requires=[
+        'pyusb'
+    ],
+    package_data={
+        'pyubertooth': []
+    }
+)
